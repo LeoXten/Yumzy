ab9ddcddf4ec83e2ef0e88af9b91d5555171fd33
Debanjan Ghosh <147796104+LeoXten@users.noreply.github.com>
2025-08-31 00:56:27 +0530

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -8,7 +8,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 //Placing user order from frontend
 const placeOrder = async (req, res) => {
 
-    const frontend_url = "http://localhost:5174"; // Ensure this matches your frontend URL
+    const frontend_url = "https://yumzy-frontend-kafv.onrender.com/"; // Ensure this matches your frontend URL
 
     try {
         const newOrder = new orderModel({
